c2242921fec1d226f8a3482a8112612be10e9bff
Hookyns <hookcze@gmail.com>
2022-01-25 08:18:10 +0100

    metadata lib TODO

--- transformer/src/getTypeDescription.ts
+++ transformer/src/getTypeDescription.ts
@@ -296,7 +296,7 @@ export function getTypeDescription(
 		if (typeCtor)
 		{
 			properties.ctor = createCtorGetter(typeCtor);
-			context.addTypeCtor(typeCtor);
+			context.addTypeCtor(typeCtor); // TODO: Check if it has modifier "export". If yes, pass path to file and name of the exported type to the addTypeCtor; handle this in index.ts and so on. If it is not exported (it is just local module type), don't generate this into the metadata lib file, generate it into the local file like in base mode
 		}
 	}
 
