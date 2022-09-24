58f1e67612a28ee4c5f1ff2ae789c7bb5ec70c09
Roman Jámbor <hookcze@gmail.com>
2022-09-25 00:30:58 +0200

    jest config update

--- jest.config.js
+++ jest.config.js
@@ -119,6 +119,7 @@ module.exports = {
 
 	// The root directory that Jest should scan for tests and modules within
 	// rootDir: undefined,
+	rootDir: "./tests",
 
 	// A list of paths to directories that Jest should use to search for files in
 	// roots: [
